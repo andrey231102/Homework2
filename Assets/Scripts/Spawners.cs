5bb241c974c61d7ed885d7eedbbfb7de3f7654c7
andrey231102 <73721039+andrey231102@users.noreply.github.com>
2021-06-19 01:18:29 +0300

    Spawner?

---
+++ Assets/Scripts/Spawners.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawners : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private GameObject[] spawners = new GameObject[5];
+    void Start()
+    {
+        StartCoroutine(SpawnObject(spawners));
+    }
+
+    IEnumerator SpawnObject(GameObject[] objects)
+    {
+        int spawnersNumber; 
+        var spawnTime = new WaitForSeconds(2f);
+        while (true)
+        {  
+            spawnersNumber = Random.Range(0, spawners.Length);
+            yield return spawnTime;
+            Instantiate(_enemy, spawners[spawnersNumber].transform);
+        }
+    }
+}
